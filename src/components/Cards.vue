c358699d6448d3e0692fb7b6d8785ea2dfcc0e00
Eden <gresseden@gmail.com>
2021-06-01 23:58:31 -0400

    First commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <img src="https://randomwordgenerator.com/img/picture-generator/55e8d7444e57a814f1dc8460962e33791c3ad6e04e5074417c2e7dd29744c7_640.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+      </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import BootstrapVue from 'bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(VueAxios, axios);
+
+// Agregamos la URL base de la API
+axios.defaults.baseURL = 'http://localhost:5000';
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
